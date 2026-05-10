428d75d2bfcda6a8cb89fc553be490fcf1839cf9
David Adejumo <shopsadejumo@gmail.com>
2026-05-10 14:40:52 +0100

    Fixed issues with ssid switch
    - Changed ssid switch to its own class rather than add multiple default arguments

--- custom_components/grandstream_gwn/switch.py
+++ custom_components/grandstream_gwn/switch.py
@@ -1,4 +1,4 @@
-from typing import Any, Awaitable, Callable
+from typing import Any
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
@@ -10,11 +10,6 @@ from .const import DOMAIN
 from .coordinator import GwnDataUpdateCoordinator
 from .sensor import _networks
 from gwn.constants import Constants
-
-def _build_toggle_callback(device_mac: str):
-    async def toggle_callback(ssid_switch_entity: "GwnSSIDSwitch", value: bool) -> bool:
-        return await ssid_switch_entity.coordinator.async_set_ssid_value(int(ssid_switch_entity._ssid[Constants.SSID_ID]), int(ssid_switch_entity._ssid[Constants.NETWORK_ID]), ssid_switch_entity._key, {device_mac: value})
-    return toggle_callback
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
     coordinator: GwnDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
@@ -31,41 +26,66 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
             entities.append(GwnSSIDSwitch(coordinator, ssid, Constants.SSID_HIDDEN, "Hide WiFi"))
             for device in network.get(Constants.DEVICES,[]):
                 device_mac: str = device.get(Constants.MAC)
-                async def toggle_callback(self, value: bool) -> bool:
-                    return await self.coordinator.async_set_ssid_value(int(self._ssid[Constants.SSID_ID]), int(self._ssid[Constants.NETWORK_ID]), self._key, {device_mac: value})
-                entities.append(GwnSSIDSwitch(coordinator, ssid, f"{Constants.TOGGLE_DEVICE}_{device_mac}", f"Assign: {device_mac}", device_mac in ssid.get(Constants.ASSIGNED_DEVICES), _build_toggle_callback(device_mac)))
+                entities.append(GwnSSIDDeviceSwitch(coordinator, ssid, Constants.TOGGLE_DEVICE, f"Assign: {device_mac}", device_mac))
 
     async_add_entities(entities)
 
 class GwnSSIDSwitch(CoordinatorEntity[GwnDataUpdateCoordinator], SwitchEntity):
-    def __init__(self, coordinator, ssid: dict[str, Any], key: str, name_suffix: str, default_state: bool | None = None, toggle_callback: Callable[[Any, bool], Awaitable[bool]] | None = None) -> None:
+    def __init__(self, coordinator, ssid: dict[str, Any], key: str, name_suffix: str) -> None:
         super().__init__(coordinator)
         self._ssid: dict[str, Any] = ssid
         self._key: str = key
-        self._default_state: bool | None = default_state
-        self._toggle_callback: Callable[[Any, bool], Awaitable[bool]] | None = toggle_callback
         self._ssid_id: str = self._ssid[Constants.SSID_ID]
         self._name: str = self._ssid[Constants.SSID_NAME]
         self._attr_name: str = f"{self._name} {name_suffix}"
         self._attr_unique_id: str = f"{self._ssid_id}_{key}"
 
     async def _toggle_value(self, value: bool) -> bool:
-        if self._toggle_callback is None:
-            return await self.coordinator.async_set_ssid_value(int(self._ssid[Constants.SSID_ID]), int(self._ssid[Constants.NETWORK_ID]), self._key, value)
-        else:
-            return await self._toggle_callback(self, value)
+        return await self.coordinator.async_set_ssid_value(int(self._ssid[Constants.SSID_ID]), int(self._ssid[Constants.NETWORK_ID]), self._key, value)
 
     @property
     def is_on(self) -> bool:
-        if self._default_state is not None:
-            return self._default_state
-        else:
-            value = self._ssid.get(self._key)
-            if isinstance(value, bool):
-                return value
-            if isinstance(value, int):
-                return value == 1
-            return False # this is an error
+        value = self._ssid.get(self._key)
+        if isinstance(value, bool):
+            return value
+        if isinstance(value, int):
+            return value == 1
+        return False # this is an error
+
+    @property
+    def device_info(self):
+        return {
+            "identifiers": {(DOMAIN, f"ssid_{self._ssid_id}")},
+            "name": self._name,
+            "manufacturer": "Grandstream",
+            "model": self._ssid.get(Constants.NETWORK_NAME, "GWN SSID"),
+        }
+
+    async def async_turn_on(self, **kwargs) -> None:
+        await self._toggle_value(True)
+
+    async def async_turn_off(self, **kwargs) -> None:
+        await self._toggle_value(False)
+
+
+class GwnSSIDDeviceSwitch(CoordinatorEntity[GwnDataUpdateCoordinator], SwitchEntity):
+    def __init__(self, coordinator, ssid: dict[str, Any], key: str, name_suffix: str, device_mac: str) -> None:
+        super().__init__(coordinator)
+        self._ssid: dict[str, Any] = ssid
+        self._key: str = key
+        self._device_mac: str = device_mac
+        self._ssid_id: str = self._ssid[Constants.SSID_ID]
+        self._name: str = self._ssid[Constants.SSID_NAME]
+        self._attr_name: str = f"{self._name} {name_suffix}"
+        self._attr_unique_id: str = f"{self._ssid_id}_{key}_{self._device_mac}"
+
+    async def _toggle_value(self, value: bool) -> bool:
+        return await self.coordinator.async_set_ssid_value(int(self._ssid[Constants.SSID_ID]), int(self._ssid[Constants.NETWORK_ID]), self._key, {self._device_mac: value})
+
+    @property
+    def is_on(self) -> bool:
+        assigned = self._ssid.get(Constants.ASSIGNED_DEVICES, {})
+        return isinstance(assigned, dict) and self._device_mac in assigned
 
     @property
     def device_info(self):
