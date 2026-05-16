dde4d68edd5605f5ba6aca84e918377ba524e1f3
David Adejumo <shopsadejumo@gmail.com>
2026-05-16 21:20:05 +0100

    Downgraded python
    Changed python to 3.14.2

--- meta_data/project_meta.py
+++ meta_data/project_meta.py
@@ -1,7 +1,7 @@
 
 APP_VERSION = "0.0.2"
-PYTHON_VERSION = "3.14.4"
-PYTHON_REQUIRES = ">=3.14.4"
+PYTHON_VERSION = "3.14.2"
+PYTHON_REQUIRES = ">=3.14.2"
 HOMEASSISTANT_MIN_VERSION = "2026.4.4"
 REPOSITORY_URL = "https://github.com/shopsD/homeassistant-grandstream-gwn"
 CONTAINER_URL = "ghcr.io/shopsd/homeassistant-grandstream-gwn"
